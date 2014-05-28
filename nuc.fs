5b91cdc1d2b73dd54bdb630e861ba3a4c3a03536
Carl D Hamann <carl.hamann@gmail.com>
2014-05-28 13:05:00 -0500

    mul improvement, code size *= 2/3

--- nuc.fs
+++ nuc.fs
@@ -1,6 +1,6 @@
 : rot >r swap r> swap ;
 : -rot swap >r swap r> ;
 : 2/mod dup 1 and swap rshift ;
-: mulstep -rot 2/mod swap 1- invert rot dup lshift -rot and >r swap rot r> + ;
+: mulstep dup lshift >r >r 2/mod -rot 1- invert r> and + swap r> ;
 : halfmul mulstep mulstep mulstep mulstep ;
-: * 0 halfmul halfmul -rot drop drop ;
+: * 0 -rot halfmul halfmul drop drop ;
